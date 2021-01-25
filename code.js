4f6d576c1c71739f930c2a13ba564dad50352a88
ChaimHonikman <73203522+ChaimHonikman@users.noreply.github.com>
2021-01-24 19:18:00 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,90 @@
+onEvent("enter", "click", function( ) {
+  setScreen("LINKS");
+});
+onEvent("HogwartsSorting", "click", function( ) {
+  open("https://my.wizardingworld.com/sorting-hat");
+});
+onEvent("LOGO", "click", function( ) {
+  open("https://wizardingworld.com");
+});
+onEvent("SHOP", "click", function( ) {
+  open("https://harrypottershop.com/");
+});
+onEvent("QUIZZES", "click", function( ) {
+  open("https://www.wizardingworld.com/quiz");
+});
+onEvent("DISCOVER", "click", function( ) {
+  setScreen("discoverbooks");
+});
+onEvent("BOOKSCATALOG", "click", function( ) {
+  setScreen("booksHP1");
+});
+onEvent("DISCOVER", "click", function( ) {
+  setScreen("discover");
+});
+onEvent("discover/books", "click", function( ) {
+  setScreen("discoverbooks");
+});
+onEvent("BOOKSCATALOG", "click", function( ) {
+  setScreen("booksHP1");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("booksHP2");
+});
+onEvent("backhp2", "click", function( ) {
+  setScreen("booksHP1");
+});
+onEvent("Backhp1", "click", function( ) {
+  setScreen("discoverbooks");
+});
+onEvent("backdiscoverbooks", "click", function( ) {
+  setScreen("discover");
+});
+onEvent("backlinks", "click", function( ) {
+  setScreen("STARTUP");
+});
+onEvent("backdiscover", "click", function( ) {
+  setScreen("LINKS");
+});
+onEvent("gryffinsort", "click", function( ) {
+  open("https://www.wizardingworld.com/outcome/gryffindor");
+});
+onEvent("ravensort", "click", function( ) {
+  open("https://www.wizardingworld.com/outcome/ravenclaw");
+});
+onEvent("hufflesort", "click", function( ) {
+  open("https://www.wizardingworld.com/outcome/hufflepuff");
+});
+onEvent("slythersort", "click", function( ) {
+  open("https://www.wizardingworld.com/outcome/slytherin");
+});
+onEvent("sorthf", "click", function( ) {
+  open("https://my.wizardingworld.com/sorting-hat");
+});
+onEvent("hhhhhhhaaaaarrrrrryyyyyy", "click", function( ) {
+  open("http://harrypotter.com/");
+});
+onEvent("beeeeassssttttssss", "click", function( ) {
+  open("https://www.fantasticbeasts.com");
+});
+onEvent("whfwhf", "click", function( ) {
+  open("https://www.wizardingworld.com/");
+});
+onEvent("creditsbutton", "click", function( ) {
+  setScreen("credits");
+});
+onEvent("subscribe", "click", function( ) {
+  open("https://www.youtube.com/channel/UCn20_CPs9DtIEezzBwoKMcg?sub_confirmation=1&feature=subscribe-embed");
+});
+onEvent("website", "click", function( ) {
+  open("http://wwfans.tk/");
+});
+onEvent("youtube", "click", function( ) {
+  open("https://www.youtube.com/channel/UCn20_CPs9DtIEezzBwoKMcg");
+});
+onEvent("myemail", "click", function( ) {
+  open("mailto:wwfans7@gmail.com");
+});
+onEvent("myotherchannels", "click", function( ) {
+  open("https://sites.google.com/view/wwfans/youtube-channels");
+});
